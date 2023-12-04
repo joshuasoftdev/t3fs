c5be37ff85088932ca34290488f38bde986f5fca
joshuasoftdev <123251561+joshuasoftdev@users.noreply.github.com>
2023-12-04 03:43:01 -0800

    signIn/signOut function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Clerk } from "@clerk/nextjs/server";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
