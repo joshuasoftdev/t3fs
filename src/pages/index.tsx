f89feda6a8244de345411e5f3fcccda424b543d2
joshuasoftdev <123251561+joshuasoftdev@users.noreply.github.com>
2024-01-10 05:06:10 -0800

    stack

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create{" "}
-            <span className="text-[hsl(280,100%,70%)]">T3 full stac</span> App
+            <span className="text-[hsl(280,100%,70%)]">T3 full stack</span> App
           </h1>
           <div>
             {!user.isSignedIn && (
