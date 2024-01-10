488010bb728e443c8dadfb467a620c92a769a226
joshuasoftdev <123251561+joshuasoftdev@users.noreply.github.com>
2024-01-10 05:05:25 -0800

    full

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#654395] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3 full st</span>{" "}
+            Create <span className="text-[hsl(280,100%,70%)]">T3 full sta</span>{" "}
             App
           </h1>
           <div>
